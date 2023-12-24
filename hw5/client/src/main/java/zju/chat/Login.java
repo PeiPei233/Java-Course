18081d7842c36bed545beb6aa51a7ab4c8ed1f10
PeiPei233 <2503585731@qq.com>
2023-12-24 11:02:36 +0800

    initial hw5

---
+++ hw5/client/src/main/java/zju/chat/Login.java
@@ -0,0 +1,16 @@
+package zju.chat;
+
+import javax.swing.*;
+
+public class Login {
+
+    private final JFrame frame;
+
+    public Login() {
+        frame = new JFrame("Login");
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+}
